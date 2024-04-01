ba0c7301ab776b30e0e12d5d95174de87eb78510
N1kh <nikhilrao2703@gmail.com>
2024-04-01 15:56:43 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Rou1Component } from './rou1/rou1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Rou1Component
   ],
   imports: [
     BrowserModule,
